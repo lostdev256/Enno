58ec06ab3eddc5df0538f78e04a1f19567e2ee40
lost.dev.256 <lost.dev.256@gmail.com>
2026-05-03 01:10:09 +0200

    Shift to Vite (#2)

--- src/bridge/bridge.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-    appVersion: process.versions.electron,
-    sendMessage: (channel, data) => ipcRenderer.send(channel, data)
-});
